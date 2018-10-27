b08226303b6c518f0f0f755fead46619f54f9aa1
Sammi Haj Hassine <sammi.hajhassine@gmail.com>
2018-10-27 14:44:26 +0200

    Fix dotmenu script

--- assets/js/modules/dotMenuHandler.js
+++ assets/js/modules/dotMenuHandler.js
@@ -1,18 +1,78 @@
 function addDotMenuHandler() {
+  const anchorProps = {};
+  const dotMenuListItems = document.getElementsByClassName("dotstyle-menu__list-item");
+  const sections = document.getElementsByClassName("section");
   const viewportHeight = window.innerHeight;
   const dotMenu = document.getElementById('dot_menu');
 
-  if (document.body.scrollTop > viewportHeight || document.documentElement.scrollTop > viewportHeight) {
-    dotMenu.classList.add('dotstyle-menu--scrolled');
+  setupSmoothScrolling();
+  setupDotmenuTriggers();
+
+
+  function setupDotmenuTriggers() {
+    // show and hide dotmenu via waypoint triggers
+    new Waypoint({
+      element: document.getElementById("one_of_us"),
+      handler: (direction) => {
+        if (direction === "down") {
+          dotMenu.classList.add('dotstyle-menu--scrolled');
+        } else {
+          dotMenu.classList.remove('dotstyle-menu--scrolled');
+        }
+      },
+      offset: window.innerHeight / 2
+    });
+
+    // change current class of dotmenu when scrolling through sections via
+    // waypoint triggers
+    Array.prototype.slice.call(sections).forEach((listItem) => {
+      const id = listItem.id;
+      const anchorListItem = document.querySelector(`[data-hash='#${id}']`);
+
+      // down waypoint trigger
+      new Waypoint({
+        element: listItem,
+        handler: function(direction) {
+          if (direction === "down") {
+            history.pushState(null, null, `#${id}`);
+            toggleCurrentClass();
+          }
+        },
+        offset: window.innerHeight / 2
+      });
+
+      // up waypoint trigger
+      new Waypoint({
+        element: listItem,
+        handler: function(direction) {
+          if (direction === "up") {
+            history.pushState(null, null, `#${id}`);
+            toggleCurrentClass();
+          }
+        },
+        offset: "-5%"
+      });
+    });
   }
 
-  window.onscroll = function() {
-    // console.log(dotMenu.getBoundingClientRect());
-    if (document.body.scrollTop > viewportHeight || document.documentElement.scrollTop > viewportHeight) {
-        dotMenu.classList.add('dotstyle-menu--scrolled');
-    } else {
-        dotMenu.classList.remove('dotstyle-menu--scrolled');
-    }
+  function setupSmoothScrolling() {
+    const scroll = new SmoothScroll('a[href*="#"]');
+    document.addEventListener('scrollStop', () => {
+      toggleCurrentClass();
+    }, false);
+  }
+
+  function toggleCurrentClass() {
+    const hash = window.location.hash;
+
+    [].slice.call(dotMenuListItems).forEach((listItem) => {
+      if (hash === listItem.dataset.hash) {
+        listItem.classList.add("dotstyle-menu__list-item--current");
+        return;
+      }
+
+      listItem.classList.remove("dotstyle-menu__list-item--current");
+    })
   }
 }
 
