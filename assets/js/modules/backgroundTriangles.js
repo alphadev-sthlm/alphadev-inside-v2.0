50969624a9a7c7c945bf5921b1ef7a5bfdc3f93d
Eduardo Castaneda <eduardo.castaneda25@gmail.com>
2018-10-28 00:31:44 +0200

    hidding background if mobile

--- assets/js/modules/backgroundTriangles.js
+++ assets/js/modules/backgroundTriangles.js
@@ -1,18 +1,27 @@
-function backgroundTriangles() {
+function addParallaxEffect() {
   const triangleGreen = window.document.getElementById("bg-triangle-green");
   const triangleBlue = window.document.getElementById("bg-triangle-blue");
   const triangleBlueSmall = window.document.getElementById("bg-triangle-blue-small");
   // const triangleOrange = window.document.getElementById("bg-triangle-orange");
-  addEventListener("scroll",  () => {
+
+
+  addEventListener("scroll", () => {
     const positionGreen = window.scrollY / 20;
     const positionBlue = window.scrollY / 40;
     // const positionOrange = window.scrollY / 20;
     const positionBlueSmall = window.scrollY / 5;
     triangleGreen.style.top = `-${Math.round(positionGreen)}px`;
     triangleBlueSmall.style.bottom = `${Math.round(positionBlueSmall)}px`;
-    triangleBlue.style.bottom = `${Math.round(positionBlue)}px`; 
+    triangleBlue.style.bottom = `${Math.round(positionBlue)}px`;
     // triangleOrange.style.bottom = `${Math.round(positionOrange)}px`; 
   });
+}
+
+function backgroundTriangles() {
+  const triangleGreen = window.document.getElementById("bg-triangle-green");
+
+  const isDisplayed = triangleGreen.offsetHeight > 0;
+  if (isDisplayed) addParallaxEffect();  
 }
 
 export default backgroundTriangles;
