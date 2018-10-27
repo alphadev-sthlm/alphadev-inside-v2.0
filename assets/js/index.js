9de7349b6e74d366c639dda9cdd0efb4d7ea5cfe
Eduardo Castaneda <eduardo.castaneda25@gmail.com>
2018-10-28 00:17:04 +0200

    added parallax effect with background triangles

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,10 @@ import addClickEvents from "./modules/addClickEvents";
 import toggleHidePeeps from "./modules/toggleClasses";
 import addDotMenuHandler from "./modules/dotMenuHandler";
 import arrowAnimation from "./modules/arrowAnimation";
+import backgroundTriangles from "./modules/backgroundTriangles";
 
 addClickEvents();
 toggleHidePeeps(true);
 addDotMenuHandler();
 arrowAnimation();
+backgroundTriangles();
