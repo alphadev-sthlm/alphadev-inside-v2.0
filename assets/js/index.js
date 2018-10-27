608d681afff68f023b64c261c86c3aae318fb86d
Eduardo Castaneda <eduardo.castaneda25@gmail.com>
2018-10-27 14:38:08 +0200

    added arrow animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 import addClickEvents from "./modules/addClickEvents";
 import toggleHidePeeps from "./modules/toggleClasses";
 import addDotMenuHandler from "./modules/dotMenuHandler";
+import arrowAnimation from "./modules/arrowAnimation";
 
 addClickEvents();
 toggleHidePeeps(true);
 addDotMenuHandler();
+arrowAnimation();
