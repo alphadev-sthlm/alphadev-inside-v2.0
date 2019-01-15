1fbd1d8ca859dd8e546293d96b5b695f7679a238
Olle Kallström <olle.kallstrom@gmail.com>
2019-01-15 18:30:44 +0100

    Removed obsolete reference

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
-import addClickEvents from "./modules/addClickEvents";
 import toggleHidePeeps from "./modules/toggleClasses";
 import addDotMenuHandler from "./modules/dotMenuHandler";
 import arrowAnimation from "./modules/arrowAnimation";
