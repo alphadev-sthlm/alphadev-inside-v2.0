4989eb4b974ab4509170e5154aeee2d4157bb617
Olle Kallström <olle.kallstrom@gmail.com>
2019-01-15 18:34:01 +0100

    Removed more obsolete references

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,8 @@
-import toggleHidePeeps from "./modules/toggleClasses";
 import addDotMenuHandler from "./modules/dotMenuHandler";
 import arrowAnimation from "./modules/arrowAnimation";
 import backgroundParallax from "./modules/backgroundParallax";
 import carousel from "./modules/carousel";
 
-addClickEvents();
-toggleHidePeeps(true);
 addDotMenuHandler();
 arrowAnimation();
 backgroundParallax();
